468cda99a380e04b789b539686238379596abe18
Paolo P <Paolo P>
2017-06-06 23:55:04 +0200

    trying to impleent svg component

--- src/app/svg-sheet/svg-sheet.component.ts
+++ src/app/svg-sheet/svg-sheet.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChildren} from '@angular/core';
 
 @Component({
   selector: 'app-svg-sheet',
   templateUrl: './svg-sheet.component.html',
   styleUrls: ['./svg-sheet.component.css']
 })
+
 export class SvgSheetComponent implements OnInit {
 
-  constructor() { }
+  @ViewChildren('svg') svgElement : any;
+  constructor() {
+    debugger;
+  }
 
   ngOnInit() {
   }
